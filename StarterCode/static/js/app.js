761ab67b1ccb96316e7697a2aa163055cb5a7170
cceni03 <czc5230.cc@gmail.com>
2020-10-05 00:08:33 -0400

    Update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -27,7 +27,23 @@ function buildPage(subject){
     function filterBySubject(testData){
       return testData.id == subject;
     }
-    // Plotly charts
+    var samples = data.samples;
+    var filteredSample = samples.filter(filterBySubject);
+    console.log(samples);
+    console.log(filteredSample);
+
+    var otuIds = otuIdList[0].slice(0, 10).reverse();
+    var otuIdLabelsList = filteredSample.map(sample => sample.otu_ids);
+    var otuIdList = filteredSample.map(sample => sample.otu_ids);
+    var otuSamplesList = filteredSample.map(sample => sample.sample_values);
+    var otuSamples = otuSamplesList[0].slice(0,10).reverse();
+    var TextIds = otuIds.map(id => 'OTU $ {id}');
+    console.log(otuIds);
+    console.log(filteredSample);
+    console.log(otuSamples);
+    console.log(otuTextIds);
+
+    // Plotly Charts
     // Horizonatal bar chart- orientation: "h"
     var trace1 = {
       orientation: "h",
@@ -38,9 +54,19 @@ function buildPage(subject){
 
     // Create Data Array 
     var data1 = [trace1];
+
+    // Layout
+    var layout = {
+      margin: {
+        b: 100,
+        l: 100,
+        r: 100,
+        t: 100
+      }
+    }
    
     // Plot Chart
-    Plotly.newPlot("bar", data1);
+    Plotly.newPlot("bar", data1, layout);
 
     // Panel
     // Filter data.metadata based on subject
@@ -50,10 +76,57 @@ function buildPage(subject){
 
     panel.html("");
 
-    Object.entries(meta).forEach(([key, value]) => {
-      // One idea is to append header elements (h5 or h6) of the key: value
-    })
+    var panelData = data.metadata;
+    console.log(panelData);
 
+    var filteredPanel = panelData.filter(filterBySubject);
+    console.log(filteredPanel[0]);
+
+    var table = panel.append("table");
+
+    Object.entries(filteredPanel[0]).forEach(([key, value]) => {
+      // One idea is to append header elements (h5 or h6) of the key: value
+      console.log(key);
+      console.log(value);
+
+      var cell1 = row.append("td");
+      cell1.text(key);
+
+      var cell2 = row.append("td");
+      cell2.text(value);
+
+      var row = table.append("tr");
+    })
+      // Gauge Chart
+      var washFreq = filtered[0].wfreq;
+      console.log(washFreq);
+
+      var data2 = [{
+        name: "Scrubs per Week",
+        type: "indicator",
+        mode: "gauge+number",
+        value: washFreq,
+        gauge: {
+          axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue"},
+          bar: { color: "blue"},
+          bgcolor: "white",
+          borderwidth: 0,
+          bordercolor: "gray",
+          steps: [
+            { range: [8,9], color: 'rgba(255, 255, 255, 0'}
+          ]
+      
+
+      }
+
+      }
+      ]
+
+
+
+
+
+      // Bubble Chart
 
   })
 }
